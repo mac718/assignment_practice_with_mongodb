4157e3348772cf0701342a7cc091abd4094725b1
mac718 <mac718@gmail.com>
2018-01-16 17:13:25 -0500

    finished updating exercises

--- practice.js
+++ practice.js
@@ -2,7 +2,14 @@
 // Inserting Products
 // ----------------------------------------
 
-// 1.
+// 1. Insert a product with the following properties
+
+//        name: "Hammer"
+//        price: 9.99
+//        department: "Hardware"
+//        color: "red"
+//        sales: 80
+ //       stock: 50
 
 db.products.insert({
   name: "Hammer",
@@ -13,35 +20,97 @@ db.products.insert({
   stock: 50
 });
 
-// 2. 
+// 2. Insert the following products in a single query
 
-db.products.insert({
+//        -
+//        name: "Screwdriver"
+//        price: 19.99
+//        department: "Hardware"
+//        color: "green"
+//        sales: 75
+//        stock: 50
+//        -
+//        name: "Wrench"
+//        price: 21.99
+//        department: "Hardware"
+//        color: "orange"
+//        sales: 70
+//         stock: 50
+
+
+db.products.insert([{
   name: "Screwdriver",
   price: 19.99,
   department: "Hardware",
   color: "green",
   sales: 75,
   stock: 50
-});
+},
 
-db.products.insert({
+{
   name: "Wrench",
   price: 21.99,
   department: "Hardware",
   color: "orange",
   sales: 70,
   stock: 50
-});
+}]);
 
 // ----------------------------------------
 // Updating Products
 // ----------------------------------------
 
-// 1.
-// invalid property id
+// 1. Change the department of all products in the "Hardware" department to "Hardware Tools"
 
-db.products.update({
+db.products.update(
   { department: "Hardware" },
+  { $set: { department: "Hardware Tools" } },
+  { multi: true }
+);
+
+// 2. Change the price of all products in the "Hardware Tools" department to cost $10 more than their current price
+
+db.products.update(
+  { department: "Hardware Tools" },
   { $inc: { price: 10 } },
   { multi: true }
-});
+);
+
+// 3. Update the sales of all the products in the "Hardware Tools" department to be at least 50
+
+db.products.update(
+  { department: "Hardware Tools"},
+  { $max: { sales: 50 } },
+  { multi: true }
+);
+
+// 4. Change the department of all the products in the "Hardware Tools" department to be "Hardware" again
+
+db.products.update(
+  { department: "Hardware Tool" },
+  { $set: { department: "Hardware" } },
+  { multi: true }
+);
+
+// 5. Change the price of all the products in the "Hardware" department to be $10 less than their current price
+
+db.products.update(
+  { department: "Tools" },
+  { $inc: { price: -10 } },
+  { multi: true }
+);
+
+// 6. Change the sales of all the products in the "Hardware" department to be at most 10
+
+db.products.update(
+  { department: "Hardware"},
+  { $min: { sales: 10 } },
+  { multi: true }
+);
+
+// 7. Update the first product in the "Hardware" department to have one more sale
+
+db.products.update(
+  { department: "Hardware"},
+  { $inc: { sales: 1 } }
+);
