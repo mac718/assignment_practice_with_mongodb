44089dc3f28404e4d476f3d22a1610ecdbef4dad
mac718 <mac718@gmail.com>
2018-01-16 22:04:39 -0500

    finished 'Finding Products'

--- practice.js
+++ practice.js
@@ -114,3 +114,127 @@ db.products.update(
   { department: "Hardware"},
   { $inc: { sales: 1 } }
 );
+
+// ----------------------------------------
+// Removing Products
+// ----------------------------------------
+
+// 1. Remove the first product in the "Hardware" department
+
+db.products.remove(
+  { department: "Hardware" },
+  { justOne: true }
+);
+
+// 2. Remove all products in the "Hardware" department
+
+db.products.remove(
+  { department: "Hardware" }
+);
+
+// ----------------------------------------
+// Finding Products
+// ----------------------------------------
+
+// 1. Find the names of all the products that are out of stock
+
+db.products.find(
+  {stock: 0},
+  {_id: 0, name: 1}
+);
+
+// 2. Find the stock count of all the products with a price below $100
+
+db.products.find(
+  {price: {$lt: 100}},
+  {_id: 0, stock: 1}
+);
+
+// 3. Find the name, color and department of all the products with a price between $100 and $1000
+
+db.products.find(
+  {
+  $and: [
+    {price: {$gt: 100}},
+    {price: {$lt: 1000}}
+  ]},
+  {_id: 0, name: 1, color: 1, department: 1}
+);
+
+// 4. Find the names of all the red products
+
+db.products.find(
+  {color: "red"},
+  {_id: 0, name: 1}
+);
+
+// 5. Find only the IDs of all the red and blue products 
+
+db.products.find(
+  {
+    $or: [ 
+      {color: "red"}, 
+      {color: "blue"}
+  ]},
+  {_id: 1}
+);
+
+// 6. Find the names of all the products that are not red or blue
+
+db.products.find(
+  {
+    $and: [
+      {color: {$ne: "red"}}, 
+      {color: {$ne: "blue"}}
+  ]},
+  {_id: 0, name: 1}
+);
+
+// 7. Find the names of all the products that are not in the Sports or Games departments
+
+db.products.find(
+  {
+    $and: [
+      {department: {$ne: "Sports"}}, 
+      {department: {$ne: "Games"}}
+  ]},
+  {_id: 0, name: 1}
+);
+
+// 8. Find the name and price of all the products with names that begin with the letter F and end with the letter S and ignore case
+
+db.products.find(
+  {
+    $and: [
+      {name: {$regex: /^F.*S$/, $options: 'i'}},   
+  ]},
+  {_id: 0, name: 1}
+);
+
+// 9. Using $where, find all the product names that begin with T
+
+db.products.find(
+  { $where: "this.name[0] == 'T'" },
+  {_id: 0, name: 1}
+);
+
+// 10. Using $where, find all the product names that begin with capital F or end with lowercase S
+
+db.products.find(
+  {$where: "this.name[0] == 'F' || this.name[-1] == 's'"},
+  {_id: 0, name: 1}
+);
+
+// 11. Using $where, find all the product names that begin with capital T and have a price less than $100
+
+db.products.find(
+  {$where: "this.name[0] == 'T' && this.price < 100"},
+  {_id: 0, name: 1}
+);
+
+// 12. Using $where, find all the product names and prices of products that either start with A and have a price of at least $100 or start with B and have a price of at most $100
+
+db.products.find(
+  {$where: "(this.name[0] == 'A' && this.price >= 100) || (this.name[0] == 'B' && this.price <= 100)"},
+  {_id: 0, name: 1, price: 1}
+);
