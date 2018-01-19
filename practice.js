dc64f7234fca69676da4909d56435e0940785665
mac718 <mac718@gmail.com>
2018-01-19 18:29:25 -0500

    finished single purpose aggregation section

--- practice.js
+++ practice.js
@@ -305,3 +305,41 @@ db.products.mapReduce(
   {query: {},
    out: "Potential_revenue" }
 ).find();
+
+
+// ---------------------------------------------------------------
+// Aggregating Products With Single Purpose Aggregation Operations
+// ---------------------------------------------------------------
+
+// 1. How many products are there?
+
+db.products.count();
+
+// 2. How many products are out of stock?
+
+db.products.count({ stock: 0 });
+
+// 3. How many products are fully stocked? (100)
+
+db.products.count({ stock: 100 });
+
+// 4. How many products are almost out of stock? (>= 5)
+
+db.products.count({ stock: { $lte: 5 } });
+
+// 5. What are all the unique names of all the departments?
+
+db.products.distinct('department');
+
+// 6. What are all the unique names of product colors?
+
+db.products.distinct('color');
+
+// 7. Find the total number of out of stock products for each department.
+
+db.products.group({
+  key: { department: 1 },
+  condition: { stock: 0 },
+  reduce: function(cur, result){ result.count += 1},
+  initial: { count: 0 }
+});
