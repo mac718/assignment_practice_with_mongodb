e1187d2132c931cda26376e4a8e391be6ef6ddc8
mac718 <mac718@gmail.com>
2018-01-22 22:56:12 -0500

    finished exercises

--- practice.js
+++ practice.js
@@ -343,3 +343,73 @@ db.products.group({
   reduce: function(cur, result){ result.count += 1},
   initial: { count: 0 }
 });
+
+
+// -------------------
+// Finding Restaurants
+// -------------------
+
+// 1. Find the first 5 restaurants returning only the name
+
+db.restaurants.find({}, { _id: 0, name: 1 }).limit(5);
+
+// 2. Find the name of all restaurants with at least 1 grade of A or B
+
+db.restaurants.find( 
+  {$or: [{ 'grades.grade': 'A' }, { 'grades.grade': 'B' }]}, 
+    { _id: 0, name: 1 }
+);
+
+
+// 3. Find the name of all restaurants with at least 1 score above 20
+
+db.restaurants.find( 
+  {'grades.score': {$gte: 20}}, 
+    { _id: 0, name: 1 }
+);
+
+
+// 4. Find the unique types of cuisine in restaurants in the Bronx
+// Not sure about this one
+
+db.restaurants.distinct('cuisine');
+
+// 5. Find all the names and addresses of all the Spanish restaurants in Queens
+
+db.restaurants.find( 
+  { cuisine: 'Spanish', borough: 'Queens' }, 
+    { _id: 0, name: 1, address: 1 }
+);
+
+// 6. Find all the names and addresses of all the restaurants in Manhattan that are not a Bakery, Spanish, Italian or Irish
+// Not sure about this one
+
+db.restaurants.find( , 
+  borough: 'Manhattan'}, 
+    { _id: 0, name: 1, address: 1 }
+);
+
+// 7. Find the name and address of the first alphabetically named Asian restaurant a grade of A
+
+db.restaurants.find(
+  { cuisine: 'Asian', 'grades.grade': 'A', name: { $regex: /^[a-zA-Z]/ }}, 
+    { _id: 0, name: 1, address: 1 }
+).sort({name: 1}).limit(1);
+
+
+// -----------------------
+// Aggregating Restaurants
+// -----------------------
+
+// 1. List the number of restaurants under each zipcode
+
+db.restaurants.aggregate({ $group: { _id: "$address.zipcode", sum: { $sum: 1 } } });
+
+// 2. List unique names of all restaurants with at least 1 grade of A or B sorted in reverse alphabetical order
+
+db.restaurants.aggregate(
+  { $match: { $or: [{'grades.grade': 'A'}, {'grades.grade': 'B'}] } },
+  { $sort: { 'name': -1 }},
+  { $project: {_id: 0, name: 1}}
+);
+
